43a6f2f7d780490de3c6b99a6bd369a35757b0e9
KimYoonMin <dbsalszz@naver.com>
2025-11-01 22:59:10 +0900

    Member 객체 추가

--- ex1-hello-jpa-start/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa-start/src/main/java/hellojpa/Member.java
@@ -3,7 +3,9 @@ package hellojpa;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
@@ -12,6 +14,11 @@ import javax.persistence.Id;
 public class Member {
 
     @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
     private String name;
+    private String city;
+    private String street;
+    private String zipcode;
 }
