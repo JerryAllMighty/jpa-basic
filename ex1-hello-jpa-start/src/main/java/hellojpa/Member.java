4762f7ead2b1f93797775aa7b7e91d2582fc8f3a
KimYoonMin <dbsalszz@naver.com>
2025-11-01 20:32:22 +0900

    Member 객체 getter setter 추가

--- ex1-hello-jpa-start/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa-start/src/main/java/hellojpa/Member.java
@@ -1,9 +1,14 @@
 package hellojpa;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
+@Getter
+@Setter
 public class Member {
 
     @Id
