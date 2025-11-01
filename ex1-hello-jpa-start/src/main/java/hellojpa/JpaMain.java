4216b9d3b19b1ab3374f9fa41f7b6791f9fbe4b5
KimYoonMin <dbsalszz@naver.com>
2025-11-01 20:33:49 +0900

    Main 함수 기본 트랜잭션 예제 추가

--- ex1-hello-jpa-start/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa-start/src/main/java/hellojpa/JpaMain.java
@@ -3,6 +3,7 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
@@ -10,8 +11,30 @@ public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
 
-        em.close();
+//        Member member = new Member();
+//        member.setId(1L);
+//        member.setName("Test");
+//
+//        em.persist(member);
+//
+//        tx.commit();
+//        em.close();
+
+        try {
+            Member foundMember = em.find(Member.class, 1L);
+            System.out.println("foundMember.id and name : " + foundMember.getId() + " ID " + foundMember.getName() + "NAME");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        }
+        finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
